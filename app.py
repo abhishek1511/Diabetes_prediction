dbcbac1bc61ed88508b304b46ae0398e7fe26a59
Abhishek <abhishek75singh99@gmail.com>
2023-12-08 16:02:35 +0530

    Adding requirements.txt and updating the app.py file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model = pickle.load(open('/Users/abhisheksingh/Desktop/Programming/Projects/DiabetesPrediction/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 #creating a function for prediction
 def diabetesPrediction(inputdata):
